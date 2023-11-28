efa340d0b9f8467eaec920dd07be6fc82f336884
Pengpa0012 <peng.ilikha@gmail.com>
2023-11-28 21:41:19 +0800

    added calculate midpoint

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
 // get relative position to each other
 // point arrow or other illustration
 
+function midpoint([x1, y1], [x2, y2]) { 
+  return [(x1 + x2) / 2, (y1 + y2) / 2]
+}
+
 function init() {
   let winPos = {
     x: undefined,
@@ -12,19 +16,25 @@ function init() {
   setInterval(() => {
     const allWin = JSON.parse(localStorage.getItem("windows")) || []
     const newWin = [...allWin]
+    const otherWin = newWin.find(el => el.id !== winPos.id)
+
     winPos = {
       ...winPos,
       x: window.screenLeft,
       y: window.screenTop,
     }
+    console.log(otherWin)
 
+    console.log("MIDPOINT", midpoint([winPos.x, winPos.y], [otherWin.x, otherWin.y]))
+
+    
     if(newWin.find(el => el.id == winPos.id)) {
       const selectWin = newWin.find(el => el.id == winPos.id)
       selectWin.x = winPos.x
       selectWin.y = winPos.y
     } else {
       newWin.push(winPos)
-    }
+    } 
 
     localStorage.setItem("windows", JSON.stringify(newWin))
   }, 1000)
@@ -35,10 +45,6 @@ function init() {
     const filteredWin = newWin.filter(el => el.id !== winPos.id)
 
     localStorage.setItem("windows", JSON.stringify(filteredWin))
-  })
-
-  addEventListener("storage", () => {
-    // update local storage from other window
   })
 } 
 
